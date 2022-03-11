1dc37d04e8627ff3289f80247b06141e1557bb19
Edilberto <edi_vazquez12@hotmail.com>
2022-03-10 20:40:52 -0600

    feat: Geolocation db getting places query [Geolocation] (#22)

--- packages/geolocation/src/index.js
+++ packages/geolocation/src/index.js
@@ -1,7 +1,9 @@
-'use strict'
+'use strict';
 
-const welcome = require("./utils/welcomePackage")
+const welcome = require('./utils/welcomePackage');
+const { Place } = require('./useCases/index');
 
 module.exports = {
-    geolocationWelcome: welcome,
-}
+  geolocationWelcome: welcome,
+  Place: Place,
+};
