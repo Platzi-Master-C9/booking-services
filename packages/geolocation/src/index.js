e0b55f7446d89518c3b4525ea7eaf5c897d4d1b4
Edilberto <edi_vazquez12@hotmail.com>
2022-03-15 00:02:02 -0600

    chore(index.js): remove welcome function from services

--- packages/geolocation/src/index.js
+++ packages/geolocation/src/index.js
@@ -1,13 +1,11 @@
 'use strict';
 
 // Services
-const welcome = require('./utils/welcomePackage');
 const { getPlaces } = require('./useCases');
 
 // Domains
 const { geoNearQuery } = require('./domains');
 
 module.exports = {
-  geolocationWelcome: welcome,
   getPlaces: getPlaces(geoNearQuery),
 };
