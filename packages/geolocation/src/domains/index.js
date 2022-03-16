e6bab5668c08765251d0aeb05ed46c65f58fe671
Edilberto <edi_vazquez12@hotmail.com>
2022-03-16 15:06:16 -0600

    chore(geolocation.js, index.js): inject connection to the geoNearquery and change aggregation method to find method

--- packages/geolocation/src/domains/index.js
+++ packages/geolocation/src/domains/index.js
@@ -1,5 +1,9 @@
+// Domains
 const { geoNearQuery } = require('./geolocation');
 
+// Drivers
+const connection = require('../drivers/mongodb/connection');
+
 module.exports = {
-  geoNearQuery,
+  geoNearQuery: geoNearQuery(connection),
 };
