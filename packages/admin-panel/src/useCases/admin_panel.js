b1adefcc7b9393ae7d1727152cbba2dfa5db5146
jacorreamed <jacorreamed@gmail.com>
2022-03-19 14:36:21 -0500

    feat(adminPanel): adding folder to admin panel package

---
+++ packages/admin-panel/src/useCases/admin_panel.js
@@ -0,0 +1,5 @@
+'use strict'
+
+const testConnection = ()=>{
+    return "done!!"
+}
---
+++ packages/admin-panel/src/useCases/index.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const adminPanel = require('./admin_panel');
+
+module.exports = {
+    adminPanel
+}
