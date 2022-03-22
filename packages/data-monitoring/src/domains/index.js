52689c33788c3538a1187be182f4af62e22c13fa
Yael Ramírez <yaelramirezmendez@gmail.com>
2022-03-22 10:51:11 -0600

    chore(domains): Add Domains files

---
+++ packages/data-monitoring/src/domains/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const PlaceModel = require('./place');
+const BookingModel = require('./booking');
+const UserModel = require('./user');
+
+module.exports = {
+	PlaceModel,
+    BookingModel,
+    UserModel
+}
---
+++ packages/data-monitoring/src/domains/user.js
@@ -0,0 +1,1 @@
+// TODO: Implement doamin to User service
