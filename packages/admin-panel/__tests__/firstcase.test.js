40cdcbe815d1be2b2678cdbbe25503c4d75756dc
jacorreamed <jacorreamed@gmail.com>
2022-03-24 17:33:06 -0500

    fix(adminPanel): testing with given-when-then

--- packages/admin-panel/__tests__/firstcase.test.js
+++ packages/admin-panel/__tests__/firstcase.test.js
@@ -1,6 +1,12 @@
 const { firstCase } = require('../src/useCases');
 
 // test example
-test('calling firstCase.sayHello() expected to be "done!!"', () => {
-  expect(firstCase.sayHello()).toBe('done!!');
-});
+describe('Given an example of use Cases',()=>{
+
+  //setup
+
+  test('When executing firstCase.sayHello()"', () => {
+    expect(firstCase.sayHello()).toBe('done!!');
+  });
+
+})
