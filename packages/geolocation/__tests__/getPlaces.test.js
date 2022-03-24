541e8c0ff3812e4c05211897e734d302dce1c05a
Edilberto <edi_vazquez12@hotmail.com>
2022-03-23 19:11:58 -0600

    feat: add test to get places query method

---
+++ packages/geolocation/__tests__/getPlaces.test.js
@@ -0,0 +1,46 @@
+const boom = require('@hapi/boom');
+const { getPlaces } = require('../src/index');
+
+test('should return an array of places, given a longitude and latitude and maxDistance', async () => {
+  const places = await getPlaces(90, 180, 1000000);
+  expect(places).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        location: expect.objectContaining({
+          type: expect.stringContaining('Point'),
+          coordinates: expect.arrayContaining([
+            expect.any(Number),
+            expect.any(Number),
+          ]),
+        }),
+        country: expect.any(String),
+        state: expect.any(String),
+        city: expect.any(String),
+        postcode: expect.any(String),
+        streetAddress: expect.any(String),
+      }),
+    ])
+  );
+});
+
+test('should return a object of type boom with 404 if any array was found', () => {
+  getPlaces(-70, 40, 0).catch((error) =>
+    expect(error).toEqual(
+      expect.objectContaining(
+        boom.notFound('[geolocation:getPlaces]: No places found', [])
+      )
+    )
+  );
+});
+
+test('should return an object of type boom with 400 if lon and lat are not supplied', () => {
+  getPlaces().catch((error) =>
+    expect(error).toEqual(
+      expect.objectContaining(
+        boom.badRequest(
+          `[geolocation:getPlaces]: latitude and longitude are required`
+        )
+      )
+    )
+  );
+});
