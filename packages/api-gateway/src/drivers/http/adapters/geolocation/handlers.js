c9b0d461955dc6f2ac180cfa7594922b48b74d20
cetherdev <robert.hct@gmail.com>
2022-03-25 20:00:27 -0600

    chore: changes in tests

--- packages/api-gateway/src/drivers/http/adapters/geolocation/handlers.js
+++ packages/api-gateway/src/drivers/http/adapters/geolocation/handlers.js
@@ -1,28 +1,32 @@
-async function getAddress(req, reply) {
-  const { lon, lat } = req.query;
-
-  req.log.info("[http-server]: Getting places: ", { lon, lat });
-
-  //const places = await this.geolocationServices.getAddress({ lon, lat });
-  const address = {
-    address: {
-      placeName: "New loft Parque",
-      placeAddress: "1600 Amphitheatre Parkway, Mountain View, CA",
-    },
-  };
-
-  if (!address) {
-    return reply
-      .code(404)
-      .header("Content-Type", "application/json; chartset:utf-8")
-      .send({ message: "Cannot get adddress" });
-    //throw new Error('Error to get address!');
+function errorHandler(error, reply) {
+  if (error.isBoom) {
+    return reply.send(error.output.payload);
   }
 
-  return reply
-    .code(200)
-    .header("Content-Type", "application/json; chartset:utf-8")
-    .send(address);
+  return reply.code(500).send({ error: error.message, stack: error.stack });
+}
+
+async function getAddress(req, reply) {
+  try {
+    const { lon, lat } = req.query;
+
+    req.log.info("[http-server]: Getting places: ", { lon, lat });
+
+    //const places = await this.geolocationServices.getAddress( lon, lat );
+    const address = {
+      address: {
+        placeName: "New loft Parque",
+        placeAddress: "1600 Amphitheatre Parkway, Mountain View, CA",
+      },
+    };
+
+    return reply
+      .code(200)
+      .header("Content-Type", "application/json; chartset:utf-8")
+      .send(address);
+  } catch (error) {
+    errorHandler(error, reply);
+  }
 }
 
 module.exports = {
