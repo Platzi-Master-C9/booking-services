1e5eedb1d9e3b9beab6808ea5908925b787bc446
Andrés Correa <60723036+jacorreamed@users.noreply.github.com>
2022-03-27 07:55:23 -0500

    Feat/create admin panel package (#122)
    * feat(adminPanel): adding folder to admin panel package
    
    * feat(adminPanel): expose service
    
    * feat(adminPanel): exposing service
    
    * feat(adminPanel) including linter and jest
    
    * fix(Math) editing line that block admin panel package
    
    * feat(adminPanel) adding domains and drivers folders
    
    * fix(adminPanel): testing with given-when-then

--- packages/api-gateway/src/drivers/http/adapters/index.js
+++ packages/api-gateway/src/drivers/http/adapters/index.js
@@ -2,8 +2,10 @@
 
 const mathAdapters = require('./math/handlers');
 const statusAdapters = require('./status/handlers');
+const adminPanelAdapters = require('./admin_panel/handlers');
 
 module.exports = {
 	mathAdapters,
 	statusAdapters,
+	adminPanelAdapters,
 }
