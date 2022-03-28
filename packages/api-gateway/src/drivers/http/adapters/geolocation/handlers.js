88dc45eac2e3c39be4956076c4efba3cfe7869a3
cetherdev <robert.hct@gmail.com>
2022-03-27 21:36:55 -0600

    chore(geolocation/handlers.js):Faker api added to endpoint responses

--- packages/api-gateway/src/drivers/http/adapters/geolocation/handlers.js
+++ packages/api-gateway/src/drivers/http/adapters/geolocation/handlers.js
@@ -1,42 +1,49 @@
-async function getPlaces(req, reply) {
-  const { lon, lat, radius } = req.query;
+"use strict";
+const { faker } = require("@faker-js/faker");
 
-  req.log.info("[http-server]: Getting places: ", { lon, lat, radius });
-
-  //const places = await this.geolocationServices.getPlaces({ lon, lat });
-  const places = [
-    {
-      placeId: "4f7df24s43",
-      placeName: "New loft Parque 1",
-      lat: 45.12346,
-      lon: 47.35332,
-    },
-    {
-      placeId: "09d8fwdf2",
-      placeName: "New loft Parque 2",
-      lat: 31.89201,
-      lon: 32.16765,
-    },
-    {
-      placeId: "98asd791d",
-      placeName: "New loft Parque 3",
-      lat: 11.54765,
-      lon: 12.56736,
-    },
-  ];
-
-  if (!places) {
-    return reply
-      .code(404)
-      .header("Content-Type", "application/json; chartset:utf-8")
-      .send({ message: "Cannot set places" });
-    //throw new Error('Error to get places!');
+function errorHandler(error, reply) {
+  if (error.isBoom) {
+    return reply.send(error.output.payload);
   }
 
-  return reply
-    .code(200)
-    .header("Content-Type", "application/json; chartset:utf-8")
-    .send(places);
+  return reply.code(500).send({ error: error.message, stack: error.stack });
+}
+
+async function getPlaces(req, reply) {
+  try {
+    const { lon, lat, radius } = req.query;
+
+    req.log.info("[http-server]: Getting places: ", { lon, lat, radius });
+
+    //const places = await this.geolocationServices.getPlaces({ lon, lat });
+    const places = [
+      {
+        placeId: faker.datatype.uuid(),
+        placeName: faker.address.secondaryAddress(),
+        lat: faker.address.latitude(),
+        lon: faker.address.longitude(),
+      },
+      {
+        placeId: faker.datatype.uuid(),
+        placeName: faker.address.secondaryAddress(),
+        lat: faker.address.latitude(),
+        lon: faker.address.longitude(),
+      },
+      {
+        placeId: faker.datatype.uuid(),
+        placeName: faker.address.secondaryAddress(),
+        lat: faker.address.latitude(),
+        lon: faker.address.longitude(),
+      },
+    ];
+
+    return reply
+      .code(200)
+      .header("Content-Type", "application/json; chartset:utf-8")
+      .send(places);
+  } catch (error) {
+    errorHandler(error, reply);
+  }
 }
 
 module.exports = {
