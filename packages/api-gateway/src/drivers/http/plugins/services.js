e13324274ddefa41c73639bf220a62697d4ad323
hinojsa <osvaldo.javier14@gmail.com>
2022-03-29 17:14:38 -0600

    added to the gitignore the '.env' file and created the auth package

--- packages/api-gateway/src/drivers/http/plugins/services.js
+++ packages/api-gateway/src/drivers/http/plugins/services.js
@@ -2,6 +2,7 @@
 
 const MathServices 		= require('@booking-services/math');
 const AdminPanelService = require('@booking-services/admin-panel')
+// const AuthServices = require("@booking-services/auth")
 
 const PluginLoader = require('fastify-plugin');
 
@@ -9,6 +10,8 @@ async function services(fastify) {
 
 	await fastify.decorate('mathServices', MathServices)
 	await fastify.decorate('adminPanelService', AdminPanelService)
+	// await fastify.decorate('authService', AuthServices)
+
 }
 
 module.exports = PluginLoader(services);
