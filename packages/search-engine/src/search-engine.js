6b71b558005c5549cc0b5630220dab83611e5504
JackoDev <untaljacko@gmail.com>
2022-03-29 17:14:49 -0500

    Create Place model and schema for the api search, and driver for db connection to MongoDB Atlas cluster

--- packages/search-engine/src/search-engine.js
+++ packages/search-engine/src/search-engine.js
@@ -1,7 +1,36 @@
-'use strict';
+const fastify = require('fastify');
+const mongoose = require('mongoose');
 
 function searchEngine() {
-    // TODO
+  // TODO
 }
+
+// initialized Fastify App PENDING HIDE CONNECTION DATA
+const app = fastify();
+const mongoURI = 'mongodb+srv://jackodev:SashaDev2023$@first-step.z7r13.mongodb.net/searchEngineDB?retryWrites=true&w=majority';
+
+// connected fastify to mongoose
+try {
+  mongoose.connect(mongoURI);
+  console.log('DB Connected...');
+} catch (e) {
+  console.error(e);
+}
+
+app.get('/', (request, reply) => {
+  try {
+    reply.send('Welcome to the search');
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+app.listen(5000, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server running on ${address}`);
+});
 
 module.exports = searchEngine;
