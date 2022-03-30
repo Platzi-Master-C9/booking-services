be2076c60e863d9b51c4e9952d4cdda89b1382c9
malonr <marlon.ramirezlizcano@gmail.com>
2022-03-30 15:55:06 -0500

    Add basic config of project

---
+++ packages/user-acount/src/drivers/postgres/connection.js
@@ -0,0 +1,13 @@
+const { Pool } = require ('pg');
+
+const { config } = require('./../../../config/config');
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
+
+const pool = new Pool({connectionString: URI});  
+
+ 
+module.exports = pool;
