ad0318898464a45a35a052acb75c5313e209a398
azanez <dasl.672004@gmail.com>
2022-03-30 00:15:20 -0500

    Initial integration of notifications module with the API Gateway

--- packages/api-gateway/src/drivers/http/adapters/index.js
+++ packages/api-gateway/src/drivers/http/adapters/index.js
@@ -2,8 +2,10 @@
 
 const mathAdapters = require('./math/handlers');
 const statusAdapters = require('./status/handlers');
+const notificationsAdapters = require('./notifications/handlers');
 
 module.exports = {
 	mathAdapters,
 	statusAdapters,
+	notificationsAdapters,
 }
