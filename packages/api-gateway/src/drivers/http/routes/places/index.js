b3591a5c8b2bc4a98d824655b2d22d5d3db58d0f
Alejandro Cuartas Marin <cuartasmarin734@gmail.com>
2022-04-01 16:03:54 -0500

    fix(api-gateway): typo

--- packages/api-gateway/src/drivers/http/routes/places/index.js
+++ packages/api-gateway/src/drivers/http/routes/places/index.js
@@ -1,8 +1,8 @@
-const { places } = require('../../adapters');
+const { placesAdapters } = require('../../adapters');
 const schema = require('./schema');
 
 async function placesRoute(fastify) {
-	await fastify.post('/', { schema }, places.places);
+	await fastify.post('/', { schema }, placesAdapters.places);
 }
 
 module.exports = placesRoute;
