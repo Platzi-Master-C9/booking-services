9bc49f0c5a987741fb47bf52d91e80cd6832f4ef
Pedro Herrera <pedro.herrera.bazaes@gmail.com>
2022-04-05 17:09:17 -0400

    chore(user): add validate user schema

--- packages/api-gateway/src/drivers/http/routes/user/schema.js
+++ packages/api-gateway/src/drivers/http/routes/user/schema.js
@@ -23,6 +23,45 @@ const createUserSchema = {
   },
 };
 
+const validateUserSchema = {
+  description: "validate user",
+  tags: ["Users"],
+  params: {
+    type: "object",
+    required: ["userId"],
+    properties: {
+      userId: { type: "string", },
+    },
+  },
+  body: {
+      type: "object",
+      properties: {
+            firstName: { type: "string" },
+            secondName: { type: "string" },
+            firstSurname: { type: "string" },
+            secondName: { type: "string" },
+            birthDate: { type: "string" },
+            nationality: { type: "string" },
+            dniId: { type: "string" },
+            dniFrontImg: { type: "string" },
+            dniBackImg: { type: "string" },
+            gender: { type: "string" },
+            phoneNumbre: { type: "string" },
+            emergencyNumber: { type: "string" },
+            passport: { type: "string" },
+            address: {
+              country: { type: "string" },
+              city: { type: "string" },
+              state: { type: "string" },
+              address: { type: "string" },
+              zip: { type: "string" }
+            },
+        },
+      required: ["firstName", "firstSruname", "birthDate", "nationality", "dniId", "dniFrontImg", "dniBackImg", "gender", "address"]
+  },
+}
+
 module.exports = {
   createUserSchema,
+  validateUserSchema,
 };
