d8d76e513247b6adf33304ac2c2c7961687e6728
Pedro Herrera <pedro.herrera.bazaes@gmail.com>
2022-04-05 17:10:55 -0400

    chore(user): add validate user router

--- packages/api-gateway/src/drivers/http/routes/user/index.js
+++ packages/api-gateway/src/drivers/http/routes/user/index.js
@@ -1,8 +1,9 @@
 const { userAdapter } = require('../../adapters');
 const { createUserSchema } = require('./schema');
 
-async function createUserRouter(fastify) {
+async function UserRouter(fastify) {
   await fastify.post('/', { schema: createUserSchema }, userAdapter.createUser);
+  await fastify.post("/:userId/validate", { validateUserSchema }, userAdapter.validateUser);
 }
 
-module.exports = createUserRouter;
+module.exports = {UserRouter};
