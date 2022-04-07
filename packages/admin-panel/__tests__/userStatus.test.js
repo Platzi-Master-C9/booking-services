f74ee98d989ddf93e1c5da1d542348284975ff63
jacorreamed <jacorreamed@gmail.com>
2022-04-06 19:33:22 -0500

    feat(admin panel): change status function

--- packages/admin-panel/__tests__/userStatus.test.js
+++ packages/admin-panel/__tests__/userStatus.test.js
@@ -7,8 +7,20 @@ describe('Given an request for changing user status',()=>{
 
   });
 
-  test('When calling  userStatus() with valid user_id, status and reason"', () => {
-    expect(userStatus(userId, status,reason)).toBe('done!!');
+  test('When calling  userStatus.changeUserStatus() with valid user_id, status and reason"', () => {
+    expect(userStatus.changeUserStatus(1, "BANNED","'cause I'm mean")).toBe('ok');
+  });
+
+  test('When calling  userStatus.changeUserStatus() with invalid user_id"', () => {
+    expect(() => userStatus.changeUserStatus(10, "BANNED","'cause I'm mean")).toThrow(Error);
+  });
+
+  test('When calling  userStatus.changeUserStatus() with invalid status"', () => {
+    expect(() => userStatus.changeUserStatus(10, "THIS_STATUS_DOES_NOT_EXIST","'cause I'm mean")).toThrow(Error);
+  });
+
+  test('When calling  userStatus.changeUserStatus() with invalid reason data type"', () => {
+    expect(() => userStatus.changeUserStatus(10, "BANNED",1)).toThrow(Error);
   });
 
 })
