e337659dbcedd317ebb719e7928897a7a84c7cea
Pedro Herrera <pedro.herrera.bazaes@gmail.com>
2022-04-07 14:17:06 -0400

    feat(user): add user validation service

--- packages/user-account/src/useCases/user.service.js
+++ packages/user-account/src/useCases/user.service.js
@@ -1,6 +1,6 @@
 const boom = require('@hapi/boom');
-const validatorHandler = require('../utils/validator/validatorHandler');
-const { createUserSchema } = require('../utils/validator/schema/user.schema');
+const validatorHandler = require("../utils/validator/validatorHandler");
+const { createUserSchema, validateUserSchema } = require("../utils/validator/schema/user.schema");
 
 function createUser(model) {
   const data = validatorHandler(createUserSchema, model);
@@ -10,6 +10,15 @@ function createUser(model) {
   return data;
 }
 
+function validateUser (model) {
+  const data = validatorHandler(validateUserSchema, model);
+  if (data != model) {
+    return boom.badRequest(data).details
+  }
+  return data;
+};
+
 module.exports = {
   createUser,
+  validateUser,
 };
