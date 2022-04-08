baddb8fb026bfaa7f6aa9a56ee86b021ab707924
jacorreamed <jacorreamed@gmail.com>
2022-04-08 08:34:34 -0500

    fix: route

--- packages/api-gateway/src/drivers/http/routes/admin-panel/index.js
+++ packages/api-gateway/src/drivers/http/routes/admin-panel/index.js
@@ -3,6 +3,6 @@ const schema = require('./schema');
 
 async function adminPanelRouter(fastify) {
   await fastify.get('/', adminPanelAdapters.sayHello);
-  await fastify.patch('/user_status/:user_id', { schema }, adminPanelAdapters.changeUserStatus);
+  await fastify.patch('/user-status/:user_id', { schema }, adminPanelAdapters.changeUserStatus);
 }
 module.exports = adminPanelRouter;
