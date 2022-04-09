a9da76fe4e5246d891f42a0c0c2eba000de320ae
efrenruizrubio <efren282@outlook.es>
2022-04-08 21:26:22 -0500

    Fixed issues and conflicts

--- packages/notifications-system/src/emailDriver/index.js
+++ packages/notifications-system/src/emailDriver/index.js
@@ -1,28 +1,27 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('dotenv').config();
 
 async function main() {
   // create reusable transporter object using the Gmail SMTP transport
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
+    host: 'smtp.gmail.com',
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
-      user: "testingbookingsystem@gmail.com", // email of the account that will be used to send the emails
-      pass: "BSTestingC92022", // password of the account (could use env variables for better security)
+      user: process.env.DEV_EMAIL_ACCOUNT, // email of the account that will send the emails
+      pass: process.env.DEV_EMAIL_PASS, // password of the account
     },
   });
 
-  // send mail with defined transport object
-  const info = await transporter.sendMail({
-    from: '"Testing Booking System" <testingbookingsystem@gmail.com>', // sender address
-    to: "efren282@outlook.es", // list of receivers
-    subject: "Testing email sending function", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
+  // send mail with defined transport object.
+  transporter.sendMail({
+    from: `Testing Booking System <${process.env.DEV_EMAIL_ACCOUNT}>`, // sender address
+    to: process.env.DEV_EMAIL_ACCOUNT, // list of receivers
+    subject: 'Testing email sending function', // Subject line
+    text: 'Hello world?', // plain text body
+    html: '<b>Hello world?</b>', // html body
   });
-
-  console.log("Message sent: %s", info.messageId);
-  // Proving that the email was sent
 }
 
-main().catch(console.error);
+main().catch();
