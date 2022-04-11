6b21611a176edb90d9df32ad09dae6c9417ee957
Alejandro Cuartas <84737789+Alejandrocuartas@users.noreply.github.com>
2022-04-11 14:19:32 -0500

    fix(api-gateway): change endpoint to '/' instead of '/create'

--- packages/api-gateway/src/drivers/http/routes/places/index.js
+++ packages/api-gateway/src/drivers/http/routes/places/index.js
@@ -2,7 +2,7 @@ const { placesAdapters } = require('../../adapters');
 const { postSchema } = require('./schema');
 
 async function placesRoute(fastify) {
-  await fastify.post('/create', { schema: postSchema }, placesAdapters.postPlace);
+  await fastify.post('/', { schema: postSchema }, placesAdapters.postPlace);
 }
 
 module.exports = placesRoute;
