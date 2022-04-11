419c451c4fab6e146dc9a23da5939f576a3409ca
Roberto Cervantes Torres <robert.hct@gmail.com>
2022-04-11 14:20:40 -0500

    feat: (GET) /geolocation/places [GEOLOCATION] (#99)
    * feat: endpoint(get) /geolocation/places [GEOLOCATION]
    
    * feat: (GET) /geolocation/places [GEOLOCATION] #14
    
    * chore: tests added to geolocation/places endpoint in api-gateway
    
    * chore(geolocation/schema.js): tags and description added to the schema
    
    * chore(geolocation/handlers.js):Faker api added to endpoint responses
    
    * fix(routes/geolocation/index.js): Fix schema composition
    
    * chore(mocks/geolocationServices.mock.js): Function for geolocation services mocked

--- packages/api-gateway/src/drivers/http/adapters/index.js
+++ packages/api-gateway/src/drivers/http/adapters/index.js
@@ -1,6 +1,6 @@
 const mathAdapters = require('./math/handlers');
-const statusAdapters = require('./status/handlers');
 const geolocationAdapters = require('./geolocation/handlers');
+const statusAdapters = require('./status/handlers');
 const adminPanelAdapters = require('./admin_panel/handlers');
 const notificationsAdapters = require('./notifications/handlers');
 const placesAdapters = require('./places/handlers');
