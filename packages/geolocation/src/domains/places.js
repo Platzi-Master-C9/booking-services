dcd1f3ffc49ed6719db324af1e78c62caf9cc7b5
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2022-04-14 11:58:09 -0500

    feat: add test to get places query method

--- packages/geolocation/src/domains/places.js
+++ packages/geolocation/src/domains/places.js
@@ -3,7 +3,7 @@ const { ObjectId } = require('mongodb');
 const updatePlaceQuery = (connection) => async (id, newValues) => {
   const options = [
     { _id: ObjectId(id) },
-    { $set: newValues, $currentDate: { deleted_at: true } },
+    { $set: newValues, $currentDate: { updated_at: true } },
   ];
   const results = await connection('updateOne', options);
   return results;
