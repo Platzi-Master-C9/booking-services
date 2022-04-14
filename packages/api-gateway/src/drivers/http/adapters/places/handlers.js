08501b77c18d88d1f7f0427d78de485dccd1fc4a
osvaldojavier14 <osvaldo.javier14@gmail.com>
2022-04-14 16:32:32 -0500

    fix: use aliases instead to avoid errors of linting in api-gateway/.../places/handlers.js

--- packages/api-gateway/src/drivers/http/adapters/places/handlers.js
+++ packages/api-gateway/src/drivers/http/adapters/places/handlers.js
@@ -1,17 +1,23 @@
 async function postPlace(req, reply) {
   const {
-    placeName, pricePerNightUsd, hostId, type,
+    place_name: placeName,
+    pricePerNightUsd: price_per_night_usd,
+    hostId: host_id,
+    type,
   } = req.body;
 
   req.log.info('[http-server]: posting a place');
 
   await this.placesService.postPlace({
-    placeName, pricePerNightUsd, hostId, type,
+    place_name: placeName,
+    pricePerNightUsd: price_per_night_usd,
+    hostId: host_id,
+    type,
   });
 
   return reply.code(201)
     .header('Content-Type', 'application/json; chartset:utf-8')
-    .send({ msg: `${placeName} has been saved correctly.` });
+    .send({ msg: `${place_name} has been saved correctly.` });
 }
 
 module.exports = {
