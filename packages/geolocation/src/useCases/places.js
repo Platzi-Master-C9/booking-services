a518c2d1cc66b5b72589297fb76c9bb37c276651
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2022-04-14 01:02:37 -0500

    chore(places.js): change boom message

--- packages/geolocation/src/useCases/places.js
+++ packages/geolocation/src/useCases/places.js
@@ -3,10 +3,10 @@ const boom = require('@hapi/boom');
 const updatePlace = (updatePlaceQuery) => async (id, streetAddress) => {
   const results = await updatePlaceQuery(id, { street_address: streetAddress });
   if (!results.matchedCount) {
-    throw boom.notFound('[geolocation:getPlaces]: No place found with id: ', id);
+    throw boom.notFound(`[geolocation:getPlaces]: No place found with id: ${id}`);
   }
   if (!results.modifiedCount) {
-    throw boom.internal('[geolocation:getPlaces]: Cannot update the place with id: ', id);
+    throw boom.internal(`[geolocation:getPlaces]: Cannot update the place with id: ${id}`);
   }
   return id;
 };
