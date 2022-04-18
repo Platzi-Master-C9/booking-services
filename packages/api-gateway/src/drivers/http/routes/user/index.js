89152e6867644cfee49b0d9ce528a91e65ef7d8d
Pedro Herrera <pedro.herrera.bazaes@gmail.com>
2022-04-18 17:24:29 -0400

    chore(api): correction config '/' schema

--- packages/api-gateway/src/drivers/http/routes/user/index.js
+++ packages/api-gateway/src/drivers/http/routes/user/index.js
@@ -2,7 +2,7 @@ const { userAdapter } = require('../../adapters');
 const { createUserSchema } = require('./schema');
 
 async function createUserRouter(fastify) {
-  await fastify.post('/', { createUserSchema }, userAdapter.createUser);
+  await fastify.post('/', { schema: createUserSchema }, userAdapter.createUser);
 }
 
 module.exports = createUserRouter;
