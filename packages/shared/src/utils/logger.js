4fe964d52a6a67d5ecd4aa64c5262e3c6797b87a
Hector Esau Macias Javier <hecttoressau@gmail.com>
2022-04-19 12:43:32 -0500

    feat(shared-package): create logger

---
+++ packages/shared/src/utils/logger.js
@@ -0,0 +1,10 @@
+const Winston = require('winston');
+
+const logger = Winston.createLogger({
+  format: Winston.format.json(),
+  transports: [
+    new Winston.transports.Console(),
+  ],
+});
+
+module.exports = logger;
