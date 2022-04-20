97d258d243e2958adc053c824bf42dade4ac45a0
Pedro Herrera <pedro.herrera.bazaes@gmail.com>
2022-04-20 12:48:22 -0400

    chore(api): fix errorHandler in user adapter

--- packages/api-gateway/src/drivers/http/adapters/users/handlers.js
+++ packages/api-gateway/src/drivers/http/adapters/users/handlers.js
@@ -1,6 +1,6 @@
 function errorHandler(error, reply) {
   if (error.isBoom) {
-    return reply.send(error.output.payload);
+    return reply.code(error.output.statusCode).send(error.output.payload);
   }
 
   return reply.code(500).send({ error: error.message, stack: error.stack });
