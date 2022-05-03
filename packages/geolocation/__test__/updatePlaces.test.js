84103f967238e115c8cf3f3e340c366bc362b239
Edilberto Vazquez Luna <edi_vazquez12@hotmail.com>
2022-05-03 00:05:36 -0500

    chore(jest.config.js, updatePlaces.test.js): use fakerjs for tests and remove unused code

--- packages/geolocation/__test__/updatePlaces.test.js
+++ packages/geolocation/__test__/updatePlaces.test.js
@@ -1,17 +1,17 @@
 const boom = require('@hapi/boom');
+const { faker } = require('@faker-js/faker');
 const { updatePlace } = require('../src/index');
 
 test('given an id and address, when need update a place, then update the place with the Id and return the id', async () => {
-  const updatedPlace = await updatePlace(
-    '62574cf309af6a16c83c8152',
-    '8971 Dudley Streets',
-  );
+  const id = faker.datatype.uuid();
+  const address = faker.address.direction();
+  const updatedPlace = await updatePlace(id, address);
   expect(updatedPlace).toEqual('62574cf309af6a16c83c8152');
 });
 
 test('given an id and address, when update a place and not found, then return a object of type boom with 404', async () => {
-  const id = '62574cf309af6a16c83c7152';
-  const address = '8971 Dudley Streets';
+  const id = faker.datatype.uuid();
+  const address = faker.address.direction();
   updatePlace(id, address)
     .catch((error) => expect(error).toEqual(
       expect.objectContaining(
@@ -21,8 +21,8 @@ test('given an id and address, when update a place and not found, then return a 
 });
 
 test('given an id and address, when a update failed, then return a object of type boom with 500', async () => {
-  const id = '62574cf309af6a16c83c7152';
-  const address = '8971 Dudley Streets';
+  const id = faker.datatype.uuid();
+  const address = faker.address.direction();
   updatePlace(id, address)
     .catch((error) => expect(error).toEqual(
       expect.objectContaining(
