229fb1893bdbacf67644f609ed3ed6e18e61f837
Pedro Herrera <pedro.herrera.bazaes@gmail.com>
2022-05-05 15:46:50 -0400

    feat(api): add new endpoint PUT /user/:userId

--- packages/api-gateway/src/drivers/http/adapters/users/handlers.js
+++ packages/api-gateway/src/drivers/http/adapters/users/handlers.js
@@ -1,7 +1,9 @@
 const createUser = require('./createUser');
 const validateUser = require('./validateUser');
+const updateUser = require('./updateUser');
 
 module.exports = {
   createUser,
   validateUser,
+  updateUser,
 };
---
+++ packages/api-gateway/src/drivers/http/adapters/users/updateUser.js
@@ -0,0 +1,92 @@
+const errorHandler = require('./errorHandler');
+
+async function updateUser(req, reply) {
+  try {
+    const {
+      email,
+      avatar,
+      firstName,
+      secondName,
+      firstSurname,
+      secondSurname,
+      birthDate,
+      nationality,
+      dniId,
+      dniFrontImg,
+      dniBackImg,
+      gender,
+      phoneNumber,
+      emergencyNumber,
+      passport,
+      address: {
+        country, city, state, address, zip,
+      },
+    } = req.body;
+    const { userId } = req.params;
+
+    req.log.info('[http-server]: Creating user with: ', {
+      userId,
+      email,
+      avatar,
+      firstName,
+      secondName,
+      firstSurname,
+      secondSurname,
+      birthDate,
+      nationality,
+      dniId,
+      dniFrontImg,
+      dniBackImg,
+      gender,
+      phoneNumber,
+      emergencyNumber,
+      passport,
+      address: {
+        country,
+        city,
+        state,
+        address,
+        zip,
+      },
+    });
+
+    const result = await this.userServices.updateUser({
+      userId,
+      email,
+      avatar,
+      firstName,
+      secondName,
+      firstSurname,
+      secondSurname,
+      birthDate,
+      nationality,
+      dniId,
+      dniFrontImg,
+      dniBackImg,
+      gender,
+      phoneNumber,
+      emergencyNumber,
+      passport,
+      address: {
+        country,
+        city,
+        state,
+        address,
+        zip,
+      },
+    });
+
+    if (result.isBoom === true) {
+      return errorHandler(result, reply);
+    }
+
+    return reply
+      .code(200)
+      .header('Content-Type', 'application/json; chartset:utf-8')
+      .send({ result });
+  } catch (error) {
+    return errorHandler(error, reply);
+  }
+}
+
+module.exports = updateUser;
