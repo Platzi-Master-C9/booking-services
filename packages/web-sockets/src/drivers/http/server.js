805bfe7beae677264e78dd8b6e2729ffe43bddfc
Hector Esau Macias Javier <hecttoressau@gmail.com>
2022-05-05 17:30:47 -0500

    feat(web-sockets): enable connection to redis

--- packages/web-sockets/src/drivers/http/server.js
+++ packages/web-sockets/src/drivers/http/server.js
@@ -1,12 +1,15 @@
 // External dependencies
+const { createAdapter } = require('@socket.io/redis-adapter');
+const { Logger } = require('@booking-services/shared');
 const { Server } = require('socket.io');
 
 // Internal dependencies
-const { Logger } = require('@booking-services/shared');
+const { pubClient, subClient } = require('../redis/connection');
 
 const io = new Server();
+io.adapter(createAdapter(pubClient, subClient));
 
-async function start() {
+function start() {
   try {
     io.listen(process.env.SERVER_PORT || 3000);
   } catch (error) {
