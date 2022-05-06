ebcc027734d4ad2920fdc52a65129c7ec530fe33
malonr <marlon.ramirezlizcano@gmail.com>
2022-05-06 09:24:40 -0500

    chore(migrations): add #3 migration currencies table

---
+++ packages/user-acount/src/domains/db/migrations/20220505194428-createCurrenciesSchema.js
@@ -0,0 +1,31 @@
+"use strict";
+const { DataTypes, Sequelize } = require("sequelize");
+const { CURRENCIES_TABLE } = require("./../models/currencies.models.js");
+
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.createTable(CURRENCIES_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      name:{
+        allowNull: false,
+        unique: true,
+        type: DataTypes.STRING
+      },
+      createdAt:{
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at',
+        defaultValue: Sequelize.NOW
+      }
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable(CURRENCIES_TABLE);
+  },
+};
