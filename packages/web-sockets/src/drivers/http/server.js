4c4d9e6710f1a09ce0508d01d0dedbd7293dd226
Hector Esau Macias Javier <hecttoressau@gmail.com>
2022-05-07 21:51:19 -0500

    fix(web-sockets): fixing the way we get socketio port from env variables

--- packages/web-sockets/src/drivers/http/server.js
+++ packages/web-sockets/src/drivers/http/server.js
@@ -5,13 +5,14 @@ const { Server } = require('socket.io');
 
 // Internal dependencies
 const { pubClient, subClient } = require('../redis/connection');
+const { port } = require('../../../config/socketio');
 
 const io = new Server();
 io.adapter(createAdapter(pubClient, subClient));
 
 function start() {
   try {
-    io.listen(process.env.SERVER_PORT || 3000);
+    io.listen(port);
   } catch (error) {
     Logger.debug(`[web-socket server]Something went wrong while enabling server ${error}`);
     process.exit(1);
