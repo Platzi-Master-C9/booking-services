454beb072c4aa3a449bcab2b71f50d2eb1e476de
hinojsa <osvaldo.javier14@gmail.com>
2022-05-13 14:40:51 -0500

    fix: typo errors

--- packages/api-gateway/src/drivers/http/adapters/auth/handlers.js
+++ packages/api-gateway/src/drivers/http/adapters/auth/handlers.js
@@ -1,7 +1,7 @@
 async function getGreeting(req, reply) {
   const result = '🤖: Hi, from a public endpoint. We are the Authentication squad 🎉🎉.';
   return reply.code(200)
-    .header('Content-Type', 'application/json; charset=utf-8')
+    .header('Content-Type', 'application/json; charset:utf-8')
     .send({ result });
 }
 
@@ -9,7 +9,7 @@ async function getPrivate(req, reply) {
   const result = '🤖: Hi, from a private endpoint! You need to be authenticated to see this.';
 
   return reply.code(200)
-    .header('Content-Type', 'application/json; charset=utf-8')
+    .header('Content-Type', 'application/json; charset:utf-8')
     .send({ result });
 }
 
@@ -17,7 +17,7 @@ async function getPrivateScoped(req, reply) {
   const result = '🤖: Hi, from a private-scoped endpoint! You need to be authenticated and have a scope of test:read:messages to see this.';
 
   return reply.code(200)
-    .header('Content-Type', 'application/json; charset=utf-8')
+    .header('Content-Type', 'application/json; charset:utf-8')
     .send({ result });
 }
 
