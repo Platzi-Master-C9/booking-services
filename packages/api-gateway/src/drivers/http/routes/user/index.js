9be460ecff67f15994819ea9ae40182b6c04d3b8
Pedro Herrera <pedro.herrera.bazaes@gmail.com>
2022-05-16 14:32:50 -0400

    feat(API): add new endpoint DELETE /user/:userId/

--- packages/api-gateway/src/drivers/http/routes/user/index.js
+++ packages/api-gateway/src/drivers/http/routes/user/index.js
@@ -1,10 +1,11 @@
 const { userAdapter } = require('../../adapters');
-const { createUserSchema, validateUserSchema, updateUserSchema } = require('./schema');
+const { createUserSchema, validateUserSchema, updateUserSchema, getUserSchema } = require('./schema');
 
 async function UserRouter(fastify) {
   await fastify.post('/', { schema: createUserSchema }, userAdapter.createUser);
   await fastify.post('/:userId/validate', { schema: validateUserSchema }, userAdapter.validateUser);
   await fastify.put('/:userId/', { schema: updateUserSchema }, userAdapter.updateUser);
+  await fastify.delete('/:userId/', { schema: getUserSchema }, userAdapter.deleteUser);
 }
 
 module.exports = UserRouter;
