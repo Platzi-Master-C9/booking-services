209b74e551f9840927bda59d5e60274d563998f6
Pedro Herrera <pedro.herrera.bazaes@gmail.com>
2022-05-16 14:26:55 -0400

    feat(API): add new function deleteUser

---
+++ packages/api-gateway/src/drivers/http/adapters/users/deleteUser.js
@@ -0,0 +1,23 @@
+const errorHandler = require('./errorHandler');
+const { getInfoUserForInfoDeleteLog } = require('./getInfoUserForInfoLog');
+
+async function deleteUser(req, reply) {
+  try {
+    req.log.info('[http-server]: Deleting user of: ', getInfoUserForInfoDeleteLog(req.params));
+
+    const result = await this.userServices.deleteUser(req.params);
+
+    if (result.isBoom) {
+      return errorHandler(result, reply);
+    }
+
+    return reply
+      .code(200)
+      .header('Content-Type', 'application/json; charset=utf-8')
+      .send({ result });
+  } catch (error) {
+    return errorHandler(error, reply);
+  }
+}
+
+module.exports = deleteUser;
