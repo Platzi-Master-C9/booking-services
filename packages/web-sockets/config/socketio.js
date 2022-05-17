8017c615b4fd0b8afce895edeb33b3bde856f52f
Hector Esau Macias Javier <hecttoressau@gmail.com>
2022-05-16 22:50:22 -0500

    feat(web-sockets): configuring socketio instance

--- packages/web-sockets/config/socketio.js
+++ packages/web-sockets/config/socketio.js
@@ -7,5 +7,7 @@ const path = require('./environment');
 dotenv.config({ path });
 
 module.exports = {
-  port: process.env.SEVER_PORT || 3001,
+  methods: process.env.WB_METHODS_CORS,
+  credentials: process.env.WB_CREDENTIALS_CORS,
+  origin: process.env.WB_ORIGIN_CORS.split(',') || '*',
 };
--- packages/web-sockets/src/drivers/http/server.js
+++ packages/web-sockets/src/drivers/http/server.js
@@ -1,25 +1,24 @@
 // External dependencies
 const { createAdapter } = require('@socket.io/redis-adapter');
-const { Logger } = require('@booking-services/shared');
 const { Server } = require('socket.io');
 
 // Internal dependencies
 const { pubClient, subClient } = require('../redis/connection');
-const { port } = require('../../../config/socketio');
+const {
+  credentials,
+  methods,
+  origin,
+} = require('../../../config/socketio');
 
-const io = new Server();
-io.adapter(createAdapter(pubClient, subClient));
+module.exports = (httpServer) => {
+  const io = new Server(httpServer, {
+    cors: {
+      origin,
+      methods,
+      credentials,
+    },
+  });
+  io.adapter(createAdapter(pubClient, subClient));
 
-function start() {
-  try {
-    io.listen(port);
-  } catch (error) {
-    Logger.debug(`[web-socket server]Something went wrong while enabling server ${error}`);
-    process.exit(1);
-  }
-}
-
-module.exports = {
-  start,
-  io,
+  return io;
 };
