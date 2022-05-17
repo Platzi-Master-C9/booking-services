b87f95753327423b533f50005fc93258fa672aa4
Hector Esau Macias Javier <hecttoressau@gmail.com>
2022-05-16 22:50:22 -0500

    feat(web-sockets): configuring socketio instance

--- packages/web-sockets/src/index.js
+++ packages/web-sockets/src/index.js
@@ -1,6 +1,5 @@
 // Internal dependencies
-const { Logger } = require('@booking-services/shared');
-const HTTPServer = require('./drivers/http/server');
 
-HTTPServer.start()
-  .catch((error) => Logger.debug(`[web-socket server] Something went wrong running server ${error}`));
+const socketioInstance = require('./drivers/http/server');
+
+module.exports = socketioInstance;
