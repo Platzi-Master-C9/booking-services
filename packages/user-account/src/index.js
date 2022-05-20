603d1b8c137c15a822ee07108d4538c99638bec1
Pedro Herrera <pedro.herrera.bazaes@gmail.com>
2022-05-20 14:41:53 -0400

    chore(user): enable createBookmark

--- packages/user-account/src/index.js
+++ packages/user-account/src/index.js
@@ -1,5 +1,5 @@
 // Services
-const { userService } = require('./useCases');
+const { userService, bookmarkService } = require('./useCases');
 
 // Domains
 const { models } = require('./drivers/postgres/sequelizer');
@@ -13,10 +13,12 @@ const {
   validateUserSchema,
   getUserSchema,
 } = require('./utils/validator/schema/user.schema');
+const { createBookmarkSchema } = require('./utils/validator/schema/bookmark.schema');
 
 module.exports = {
   createUser: validatorHandler.bind(null, userService.createUser, models, createUserSchema),
   validateUser: validatorHandler.bind(null, userService.validateUser, models, validateUserSchema),
   updateUser: validatorHandler.bind(null, userService.updateUser, models, updateUserSchema),
   deleteUser: validatorHandler.bind(null, userService.deleteUser, models, getUserSchema),
+  createBookmark: validatorHandler.bind(null, bookmarkService.createBookmark, models, createBookmarkSchema),
 };
