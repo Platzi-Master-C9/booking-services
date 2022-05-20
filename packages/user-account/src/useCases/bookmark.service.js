fc59c6a5556c7ba04432a233faaf9c6597a06107
Pedro Herrera <pedro.herrera.bazaes@gmail.com>
2022-05-20 14:40:19 -0400

    feat(user): add bookmarks services

---
+++ packages/user-account/src/useCases/bookmark.service.js
@@ -0,0 +1,37 @@
+function createBookmark(data) {
+    return data;
+};
+
+function getBookmark(data) {
+    return data;
+};
+
+function getBookmarksList(data) {
+    return data;
+};
+
+function updateBookmark(data) {
+    return data;
+};
+
+function deleteBookmark(data) {
+    return data;
+};
+
+function addItemInBookmark(data) {
+    return data;
+};
+
+function deleteItemInBookmark(data) {
+    return data;
+}
+
+module.exports = {
+    createBookmark,
+    getBookmark,
+    getBookmarksList,
+    updateBookmark,
+    deleteBookmark,
+    addItemInBookmark,
+    deleteItemInBookmark
+};
