938bdfb566eb3728ff15cb2a2c7f1ccd623a6dc1
Andrés Correa <60723036+jacorreamed@users.noreply.github.com>
2022-05-19 19:28:58 -0500

    fix(admin-panel): fixing detail in fields for admins end point (#237)

--- packages/admin-panel/src/useCases/adminList.js
+++ packages/admin-panel/src/useCases/adminList.js
@@ -3,10 +3,10 @@ const { adminList } = require('../../mocks/adminList.mock');
 const getAdmins = (profile, fullName) => {
   let fakeAdminList = adminList;
 
-  fakeAdminList = fakeAdminList.filter((user) => user.userType === 3 || user.userType === 4);
+  fakeAdminList = fakeAdminList.filter((user) => user.profile === 3 || user.profile === 4);
 
   if (profile) {
-    fakeAdminList = fakeAdminList.filter((admin) => admin.userType === profile);
+    fakeAdminList = fakeAdminList.filter((admin) => admin.profile === profile);
   }
   if (fullName) {
     fakeAdminList = fakeAdminList.filter((admin) => admin.fullName.search(fullName) >= 0);
