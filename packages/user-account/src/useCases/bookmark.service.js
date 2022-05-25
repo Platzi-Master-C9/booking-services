bdbea679a32fc5ef440fcd07311578243c0b7c52
Pedro Herrera <pedro.herrera.bazaes@gmail.com>
2022-05-25 14:11:37 -0400

    chore(user): add createBookmark set in services

--- packages/user-account/src/useCases/bookmark.service.js
+++ packages/user-account/src/useCases/bookmark.service.js
@@ -1,37 +1,45 @@
-function createBookmark(data) {
-    return data;
+const boom = require("@hapi/boom");
+
+async function createBookmark(model, data) {
+  try {
+    const newBookmark = await model.userFavoritePlaces.create(data);
+    return newBookmark;
+    
+  } catch (error) {
+    return boom.badRequest(error);
+  }
 };
 
-function getBookmark(data) {
-    return data;
-};
+function getBookmark(model, data) {
+  return data;
+}
 
-function getBookmarksList(data) {
-    return data;
-};
+function getBookmarksList(model, data) {
+  return data;
+}
 
-function updateBookmark(data) {
-    return data;
-};
+function updateBookmark(model, data) {
+  return data;
+}
 
-function deleteBookmark(data) {
-    return data;
-};
+function deleteBookmark(model, data) {
+  return data;
+}
 
-function addItemInBookmark(data) {
-    return data;
-};
+function addItemInBookmark(model, data) {
+  return data;
+}
 
-function deleteItemInBookmark(data) {
-    return data;
+function deleteItemInBookmark(model, data) {
+  return data;
 }
 
 module.exports = {
-    createBookmark,
-    getBookmark,
-    getBookmarksList,
-    updateBookmark,
-    deleteBookmark,
-    addItemInBookmark,
-    deleteItemInBookmark
+  createBookmark,
+  getBookmark,
+  getBookmarksList,
+  updateBookmark,
+  deleteBookmark,
+  addItemInBookmark,
+  deleteItemInBookmark,
 };
