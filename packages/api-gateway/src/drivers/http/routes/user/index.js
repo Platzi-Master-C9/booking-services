dbf27f50b060a4ed6cbba91318d7c70d103e53c4
Pedro Herrera <pedro.herrera.bazaes@gmail.com>
2022-05-25 14:12:42 -0400

    feat(API): add create bookmark endpoint

--- packages/api-gateway/src/drivers/http/routes/user/index.js
+++ packages/api-gateway/src/drivers/http/routes/user/index.js
@@ -1,13 +1,17 @@
 const { userAdapter } = require('../../adapters');
 const {
-  createUserSchema, validateUserSchema, updateUserSchema, getUserSchema,
+  createUserSchema, validateUserSchema, updateUserSchema, getUserSchema, createBookmarkSchema,
 } = require('./schema');
 
 async function UserRouter(fastify) {
+  // users endpoints
   await fastify.post('/', { schema: createUserSchema }, userAdapter.createUser);
   await fastify.post('/:userId/validate', { schema: validateUserSchema }, userAdapter.validateUser);
   await fastify.put('/:userId/', { schema: updateUserSchema }, userAdapter.updateUser);
   await fastify.delete('/:userId/', { schema: getUserSchema }, userAdapter.deleteUser);
+
+  // bookmark endpoints
+  await fastify.post('/:userId/bookmark', { schema: createBookmarkSchema }, userAdapter.createBookmark);
 }
 
 module.exports = UserRouter;
