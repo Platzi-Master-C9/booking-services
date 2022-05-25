2b4be7e6022fca6bdab8ec91d132600c5870e4d5
Pedro Herrera <pedro.herrera.bazaes@gmail.com>
2022-05-25 14:05:15 -0400

    feat(API): add createBookmark services in adapter

--- packages/api-gateway/src/drivers/http/adapters/users/handlers.js
+++ packages/api-gateway/src/drivers/http/adapters/users/handlers.js
@@ -4,9 +4,13 @@ const validateUser = require('./userServices/validateUser');
 const updateUser = require('./userServices/updateUser');
 const deleteUser = require('./userServices/deleteUser');
 
+// bookmark services
+const createBookmark = require('./bookmarkServices/createBookmark');
+
 module.exports = {
   createUser,
   validateUser,
   updateUser,
   deleteUser,
+  createBookmark
 };
