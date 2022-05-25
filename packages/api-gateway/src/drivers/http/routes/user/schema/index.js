ec294d53cc556d173fbe4fd9e2c69aae5c1fbf49
Pedro Herrera <pedro.herrera.bazaes@gmail.com>
2022-05-25 14:09:57 -0400

    feat(API): add createBookmarkSchema in users

--- packages/api-gateway/src/drivers/http/routes/user/schema/index.js
+++ packages/api-gateway/src/drivers/http/routes/user/schema/index.js
@@ -4,9 +4,13 @@ const validateUserSchema = require('./users/validateUserSchema');
 const updateUserSchema = require('./users/updateUserSchema');
 const getUserSchema = require('./users/getUserSchema');
 
+// bookmark schemas
+const createBookmarkSchema = require('./bookmark/createBookmarkSchema');
+
 module.exports = {
   createUserSchema,
   validateUserSchema,
   updateUserSchema,
   getUserSchema,
+  createBookmarkSchema
 };
