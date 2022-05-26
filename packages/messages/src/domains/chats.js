6939a90ebe18ae7f7e0209308faa8d87d3f9b020
Hector Esau Macias Javier <hecttoressau@gmail.com>
2022-05-25 20:37:39 -0500

    feat(mesages-models): create chat model

---
+++ packages/messages/src/domains/chats.js
@@ -0,0 +1,12 @@
+// External dependencies
+const { Schema } = require('mongoose');
+// Internal dependencies
+const connection = require('../drivers/mongodb/connection');
+
+const Chat = new Schema({
+  hostId: { type: String, required: true },
+  customerId: { type: String, required: true },
+  placeId: { type: String, required: true },
+}, { timestamps: true });
+
+module.exports = connection.model('chat', Chat);
