14c0801759f448187df3b22b9f1c02c021b7b379
cetherdev <robert.hct@gmail.com>
2022-06-07 20:37:39 -0500

    feat: create endpoint to validate an address when the host is creating a place

--- packages/api-gateway/src/drivers/http/adapters/places/handlers.js
+++ packages/api-gateway/src/drivers/http/adapters/places/handlers.js
@@ -1,83 +1,98 @@
 function errorHandler(error, reply) {
-  if (error.isBoom) {
-    return reply
-      .code(error.output.payload.statusCode)
-      .send(error.output.payload);
-  }
+	if (error.isBoom) {
+		return reply
+			.code(error.output.payload.statusCode)
+			.send(error.output.payload);
+	}
 
-  return reply.code(500).send({ error: error.message, stack: error.stack });
+	return reply.code(500).send({ error: error.message, stack: error.stack });
 }
 
 async function postPlace(req, reply) {
-  try {
-    const {
-      amenities,
-      address,
-      floorPlans,
-      title,
-      description,
-      houseRules,
-      healthAndSecurity,
-      price,
-    } = req.body;
-    req.log.info('[http-server]: posting a place');
-    const result = await this.placesService.createPlace(
-      amenities,
-      address,
-      floorPlans,
-      title,
-      description,
-      houseRules,
-      healthAndSecurity,
-      price,
-    );
-    return reply.code(201)
-      .send({ response: result });
-  } catch (error) {
-    return errorHandler(error, reply);
-  }
+	try {
+		const {
+			amenities,
+			address,
+			floorPlans,
+			title,
+			description,
+			houseRules,
+			healthAndSecurity,
+			price,
+		} = req.body;
+		req.log.info('[http-server]: posting a place');
+		const result = await this.placesService.createPlace(
+			amenities,
+			address,
+			floorPlans,
+			title,
+			description,
+			houseRules,
+			healthAndSecurity,
+			price
+		);
+		return reply.code(201).send({ response: result });
+	} catch (error) {
+		return errorHandler(error, reply);
+	}
 }
 
 async function getPlaces(req, reply) {
-  req.log.info('[http-server]: reading places');
+	req.log.info('[http-server]: reading places');
 
-  let places;
-  try {
-    places = await this.placesService.getPlaces();
-  } catch (error) {
-    return reply.code(500).send({
-      message: 'Could not get places',
-    });
-  }
+	let places;
+	try {
+		places = await this.placesService.getPlaces();
+	} catch (error) {
+		return reply.code(500).send({
+			message: 'Could not get places',
+		});
+	}
 
-  return reply.code(200).send({
-    total: places.length,
-    places,
-  });
+	return reply.code(200).send({
+		total: places.length,
+		places,
+	});
 }
 
 async function deletePlace(req, reply) {
-  const { id } = req.params;
+	const { id } = req.params;
 
-  req.log.info('[http-server]: deleting a place');
+	req.log.info('[http-server]: deleting a place');
 
-  try {
-    await this.placesService.deletePlace(id);
-  } catch (error) {
-    return reply.code(404)
-      .send({
-        message: 'Incorrect id',
-      });
-  }
+	try {
+		await this.placesService.deletePlace(id);
+	} catch (error) {
+		return reply.code(404).send({
+			message: 'Incorrect id',
+		});
+	}
 
-  return reply.code(200)
-    .send({
-      message: `The place with the id ${id} has been deleted correctly.`,
-    });
+	return reply.code(200).send({
+		message: `The place with the id ${id} has been deleted correctly.`,
+	});
+}
+
+async function validateAddress(req, reply) {
+	try {
+		const { country, state, city, zipcode, street } = req.body;
+		req.log.info('[http-server]: posting a place');
+		const result = await this.geolocationServices.geocoding(
+			country,
+			state,
+			city,
+			zipcode,
+			street
+		);
+		return reply.code(201).send({ response: result });
+	} catch (error) {
+		return errorHandler(error, reply);
+	}
 }
 
 module.exports = {
-  postPlace,
-  getPlaces,
-  deletePlace,
+	postPlace,
+	getPlaces,
+	deletePlace,
+	validateAddress,
 };
--- packages/api-gateway/src/drivers/http/routes/places/schema.js
+++ packages/api-gateway/src/drivers/http/routes/places/schema.js
@@ -1,94 +1,104 @@
 const postPlaceSchema = {
-  description: 'This is the route to use for creating a new place',
-  tags: ['Places'],
-  body: {
-    type: 'object',
-    properties: {
-      propertyType: { type: 'string', description: 'property type' },
-      amenities: { type: 'array', description: 'amenities' },
-      address: {
-        type: 'object',
-        properties: {
-          country: { type: 'string', description: 'Country' },
-          state: { type: 'string', description: 'State' },
-          city: { type: 'string', description: 'City' },
-          zipcode: { type: 'string', description: 'Zipcode' },
-          street: { type: 'string', description: 'street' },
-          price: { type: 'number', description: 'price' },
-        },
-        required: [
-          'country',
-          'state',
-          'city',
-          'zipcode',
-          'street',
-          'price',
-        ],
-      },
-      floorPlans: { type: 'array', description: 'floor plans' },
-      title: { type: 'string', description: 'place title' },
-      description: { type: 'string', description: 'place description' },
-      houseRules: { type: 'array', description: 'house rules' },
-      healthAndSecurity: { type: 'array', description: 'health and security' },
-      price: { type: 'number', description: 'price' },
-    },
-    required: [
-      'propertyType',
-      'amenities',
-      'address',
-      'floorPlans',
-      'title',
-      'description',
-      'houseRules',
-      'healthAndSecurity',
-      'price',
-    ],
-  },
+	description: 'This is the route to use for creating a new place',
+	tags: ['Places'],
+	body: {
+		type: 'object',
+		properties: {
+			propertyType: { type: 'string', description: 'property type' },
+			amenities: { type: 'array', description: 'amenities' },
+			address: {
+				type: 'object',
+				properties: {
+					country: { type: 'string', description: 'Country' },
+					state: { type: 'string', description: 'State' },
+					city: { type: 'string', description: 'City' },
+					zipcode: { type: 'string', description: 'Zipcode' },
+					street: { type: 'string', description: 'street' },
+					price: { type: 'number', description: 'price' },
+				},
+				required: ['country', 'state', 'city', 'zipcode', 'street', 'price'],
+			},
+			floorPlans: { type: 'array', description: 'floor plans' },
+			title: { type: 'string', description: 'place title' },
+			description: { type: 'string', description: 'place description' },
+			houseRules: { type: 'array', description: 'house rules' },
+			healthAndSecurity: { type: 'array', description: 'health and security' },
+			price: { type: 'number', description: 'price' },
+		},
+		required: [
+			'propertyType',
+			'amenities',
+			'address',
+			'floorPlans',
+			'title',
+			'description',
+			'houseRules',
+			'healthAndSecurity',
+			'price',
+		],
+	},
 };
 
 const getPlacesSchema = {
-  description: 'Route for getting all the places',
-  tags: ['Places'],
-  response: {
-    200: {
-      description: 'Successful response',
-      type: 'object',
-      properties: {
-        total: { type: 'number' },
-        places: { type: 'array' },
-      },
-    },
-  },
+	description: 'Route for getting all the places',
+	tags: ['Places'],
+	response: {
+		200: {
+			description: 'Successful response',
+			type: 'object',
+			properties: {
+				total: { type: 'number' },
+				places: { type: 'array' },
+			},
+		},
+	},
 };
 
 const deletePlaceSchema = {
-  description: 'This is the route to use for deleting a place',
-  tags: ['Places'],
-  params: {
-    type: 'object',
-    properties: {
-      id: { type: 'number' },
-    },
-    required: ['id'],
-  },
-  response: {
-    200: {
-      type: 'object',
-      properties: {
-        message: { type: 'string' },
-      },
-    },
-    404: {
-      type: 'object',
-      properties: {
-        message: { type: 'string' },
-      },
-    },
-  },
+	description: 'This is the route to use for deleting a place',
+	tags: ['Places'],
+	params: {
+		type: 'object',
+		properties: {
+			id: { type: 'number' },
+		},
+		required: ['id'],
+	},
+	response: {
+		200: {
+			type: 'object',
+			properties: {
+				message: { type: 'string' },
+			},
+		},
+		404: {
+			type: 'object',
+			properties: {
+				message: { type: 'string' },
+			},
+		},
+	},
+};
+
+const validateAddressSchema = {
+	description: 'This is the route to use for validate a new place',
+	tags: ['Places'],
+	body: {
+		type: 'object',
+		properties: {
+			country: { type: 'string', description: 'Country' },
+			state: { type: 'string', description: 'State' },
+			city: { type: 'string', description: 'City' },
+			zipcode: { type: 'string', description: 'Zipcode' },
+			street: { type: 'string', description: 'street' },
+		},
+		required: ['country', 'state', 'city', 'zipcode', 'street'],
+	},
 };
 
 module.exports = {
-  postPlaceSchema,
-  getPlacesSchema,
-  deletePlaceSchema,
+	postPlaceSchema,
+	getPlacesSchema,
+	deletePlaceSchema,
+	validateAddressSchema,
 };
